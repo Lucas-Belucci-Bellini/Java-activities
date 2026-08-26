4f7999ce16de99d936f637af89f122f9b6a653ca
Lucas Belucci Bellini <lucasbb2007@gmail.com>
2026-08-26 00:53:21 +0000

    tinha botado um nome de carro e me arrependi

--- ATV 9/NOVEMBER/Main.java
+++ ATV 9/NOVEMBER/Main.java
@@ -4,16 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Criando um carro.
-        // O motor é criado junto com o carro.
+       
         Carro carro = new Carro(
-                "Toyota",
-                "Corolla",
+                "Toyota Supra",
+                "Nissan Sky",
                 2.0,
                 "Flex"
         );
 
-        // Apresentando os dados
+        
         carro.apresentarCarro();
     }
 }
